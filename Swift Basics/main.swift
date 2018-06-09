a328c1eca0507618d8efdd89aeba99a178fa290e
Ethan Schutzenhofer <ews624@mail.missouri.edu>
2018-06-09 12:22:25 -0500

    Saved
    First Save

--- Swift Basics/main.swift
+++ Swift Basics/main.swift
@@ -15,3 +15,18 @@ var value = 3.58
 var age = 21
 print("age = \(age)")
 
+
+let sample1 : UInt8 = 0x3A
+var sample2 : UInt8 = 58
+var heartRate : Int = 85
+var deposits : Double = 135002796
+let acceleration : Float = 9.800
+var mass : Float = 14.6
+var distance : Double = 129.763001
+var lost : Bool = true
+var choice : Int = 2
+let integral : Character = '\u{222B}'
+let greeting : String = "Hello"
+var name : String = "Karen"
+
+
