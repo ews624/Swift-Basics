33deb6b464a30869427d13811b5f3e7ec221c165
Ethan Schutzenhofer <ews624@mail.missouri.edu>
2018-06-09 12:06:46 -0500

    Initial commit

---
+++ Swift Basics/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  Swift Basics
+//
+//  Created by Ethan Schutzenhofer on 6/9/18.
+//  Copyright © 2018 Ethan Schutzenhofer. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+var value = 3.58
+
+var age = 21
+print("age = \(age)")
+
